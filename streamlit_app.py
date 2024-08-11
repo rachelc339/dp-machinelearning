5bcb60c254fe7c9db9b1a5c047be303d387df6d8
rachelc339 <115439639+rachelc339@users.noreply.github.com>
2024-08-11 16:03:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,6 @@ st.title('Machine Learning App')
 st.info('This is app builds a machine learning model!')
 
 df = pd.read_csv('https://github.com/dataprofessor/data/blob/c2b6c1fb592c9d7a07e6a08a5bf417209f4b1400/penguins_cleaned.csv')
-df
+print(df)
 
 
